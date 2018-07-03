67335cebd1f882587945c9bc5ac9b5997d556ebe
Rob Bovill <rbovill@lsst.org>
2018-07-03 13:59:09 -0700

    Added a script to generate the list of test suites to run on Jenkins. Ran it to pick up recent changes, namely, the updates for the CalibrationElectrometer.

---
+++ scripts/Jenkins_Testlist_SalGen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#  Shellscript to create test suite argument file
+#  for the salgenerator tests.
+#
+#  author: Rob Bovill
+#  email:  rbovill@lsst.org
+
+# Define filename
+argfile=$HOME/trunk/robotframework_salgenerator/Jenkins_SalGen_Test.list
+
+# Find and remove old argument file.
+ls $argfile 1>/dev/null
+if [ $? -eq 0 ]; then
+	echo "Deleteing $argfile"
+	rm $argfile
+else
+	echo "$argfile does not exist.  Continuing."
+fi
+
+# Setup argument file
+echo "###  Test Suite list ###" >> $argfile
+echo "#  Change the title of the test results" >> $argfile
+echo "--name JenkinsSalGen" >> $argfile
+echo "--output SALGENERATOR" >> $argfile
+echo "" >> $argfile
+echo "# List of test suites" >> $argfile
+echo "rbtsal/Version.robot" >> $argfile
+
+# Find test suites and add them to the argument file.
+ls -1 $HOME/trunk/robotframework_salgenerator/SALGEN |sed 's/^/rbtsal\/SALGEN\//' >> $argfile
