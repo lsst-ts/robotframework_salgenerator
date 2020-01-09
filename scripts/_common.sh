403a9e795ca9ff546f9d9ec7a5f208cacad0fce1
rbovill <rbovill@lsst.org>
2020-01-09 15:11:44 -0700

    Updated the list of CSCs and deleted the OCS_salgenerator.robot test suite, since the CSC has been removed.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -50,7 +50,19 @@ function clearTestSuites() {
 }
 
 function subsystemArray() {
-	echo "ATAOS ATArchiver ATBuilding ATCamera ATDome ATDomeTrajectory ATHeaderService ATHexapod ATMCS ATMonochromator ATPneumatics ATPtg ATSpectrograph ATTCS ATWhiteLight CBP CatchupArchiver DIMM Dome DSM EAS EFD EFDTransformationServer Electrometer Environment FiberSpectrograph GenericCamera HVAC Hexapod IOTA LOVE LinearStage MTAOS MTArchiver MTCamera MTDomeTrajectory MTEEC MTGuider MTHeaderService MTLaserTracker MTM1M3 MTM1M3TS MTM2 MTMount MTPtg MTTCS MTVMS OCS PointingComponent PromptProcessing Rotator Scheduler Script ScriptQueue SummitFacility Test TunableLaser Watcher"
+    echo "ATAOS ATArchiver ATBuilding ATCamera ATDome
+    ATDomeTrajectory ATHeaderService ATHexapod ATMCS
+    ATMonochromator ATPneumatics ATPtg ATSpectrograph ATTCS
+    ATWhiteLight CCArchiver CCCamera CCHeaderService CatchupArchiver
+    CBP DIMM Dome
+    DSM EAS EFD EFDTransformationServer Electrometer Environment
+    FiberSpectrograph GenericCamera IOTA Hexapod HVAC
+    LinearStage LOVE MTAOS MTArchiver MTCamera
+    MTDomeTrajectory MTEEC MTGuider MTHeaderService
+    MTLaserTracker MTM1M3 MTM1M3TS MTM2 MTMount MTPtg MTTCS
+    MTVMS PointingComponent PromptProcessing Rotator
+    Scheduler Script ScriptQueue SummitFacility Test
+    TunableLaser Watcher"
 }
 
 function stateArray() {
