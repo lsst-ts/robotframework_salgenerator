f9d247cf6afe4842082fcd34a3b1162165cd923c
Rob Bovill <rbovill@lsst.org>
2020-10-20 17:16:16 -0700

    Deleted the MTArchiver tests. That CSC was removed.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -95,7 +95,7 @@ function subsystemArray() {
     EAS Electrometer Environment ESS
     FiberSpectrograph GenericCamera IOTA 
     Hexapod HVAC LinearStage LOVE 
-    MTArchiver MTAOS MTAlignment MTCamera
+    MTAOS MTAlignment MTCamera
     MTDomeTrajectory MTEEC MTHeaderService
     MTM1M3 MTM1M3TS MTM2 MTMount MTPtg MTVMS
     NewMTMount PromptProcessing Rotator
