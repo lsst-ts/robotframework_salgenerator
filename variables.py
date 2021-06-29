81c38fb29517791988a931c7fb401d9c80d5cc8b
Rob Bovill <rbovill@lsst.org>
2021-06-29 16:54:48 -0700

    Deleted random_value.py, which was left over from the Messaging test split. Removed unused functions from _common.sh.
    Removed the getEntity function, as it is now redundant.
    
    Added function documentation to the functions in _common.sh.
    
    Added function documentation and made formatting improvements in SalGen.sh.
    
    Added a comment heading to variables.py.
    
    Archived three Jenkinsfiles, as they are not used anymore, but should be kept around for a bit longer, just in case.

--- variables.py
+++ variables.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This library determines the OS Distribution
+#  value, which is used in the RPM naming
+#  strategy.
+# It is imported by Global_Vars.resource.
+#
 f = open("/etc/rpm/macros.dist")
 array = f.read().split()
 DIST = array[10]
