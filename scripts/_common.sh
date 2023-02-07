c5fd5ac99e7185230c65d5990c549049578733a0
rbovill <rbovill@lsst.org>
2023-02-07 12:45:13 -0700

    Removed all references to ATArchiver and CCArchiver; those CSCs were removed.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -188,11 +188,11 @@ function subsystemArray() {
     # generate this list, it is better to have an independent list, such that
     # the thing being tested is not used to test the thing.
     #
-    echo "ATAOS ATArchiver ATBuilding ATCamera 
+    echo "ATAOS ATBuilding ATCamera 
     ATDome ATDomeTrajectory ATHeaderService ATHexapod 
     ATMCS ATMonochromator ATOODS ATPneumatics 
     ATPtg ATSpectrograph ATWhiteLight Authorize
-    CCArchiver CCCamera CCHeaderService CCOODS
+    CCCamera CCHeaderService CCOODS
     CatchupArchiver CBP DIMM DREAM DSM
     EAS Electrometer ESS FiberSpectrograph 
     GCHeaderService GenericCamera GIS Guider IOTA 
