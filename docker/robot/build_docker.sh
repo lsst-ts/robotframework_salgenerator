c431534d10f6a997bfacaea2c709fca26a3b6732
rbovill <rbovill@lsst.org>
2026-06-29 16:52:01 -0700

    Fix if/else logic in docker/robot*/build_docker.sh.

--- docker/robot/build_docker.sh
+++ docker/robot/build_docker.sh
@@ -1,8 +1,8 @@
 tag=${1-latest}
 docker build --platform linux/amd64 --tag lsstts/robot:$tag .
-if [ $? ]; then
-    echo "\n\nHere's the command to push to image: \n"
-    echo "docker image push lsstts/robot:$tag"
+if [ $? -e 0 ]; then
+    echo -e "\n\nHere's the command to push to image: \n"
+    echo -e "docker image push lsstts/robot:$tag"
 else
-    echo "Failed to build Docker image."
+    echo -e "\n\nFailed to build Docker image."
 fi
